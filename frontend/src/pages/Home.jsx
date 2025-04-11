2fc0b924ba2d6d2078c513306afd129ecd7f5a4c
gopiraman27 <gopiraman.in@gmail.com>
2025-04-12 00:10:20 +0530

    top doc section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Speciality from '../components/Speciality';
+import TopDoctors from '../components/TopDoctors';
 function Home(){
     return(
         <div>
             <Header/>
             <Speciality />
+            <TopDoctors />
         </div>
     );
 }
